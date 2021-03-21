38675d8765fa3937419d8c5116748e3454115250
dokk911 <76562544+dokk911@users.noreply.github.com>
2021-03-21 08:08:54 +0300

    Add files via upload

---
+++ Console2103Dz/Console2103Dz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2103Dz3
+{
+    class Program
+    {
+        //вырезает из строковой перемены буквы
+
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            CuttingLetters(ref text);
+
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+
+        public static void CuttingLetters(ref string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]) == true)
+                {
+                    text = text.Remove(i, 1);
+                    i--;
+                }
+            }
+        }
+    }
+}
---
+++ Console2103Dz/Console2103Dz7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2103Dz7
+{
+    class Program
+    {
+        //решает квадратное уравнение
+
+        static void Main(string[] args)
+        {
+            double x1 = 0;
+            double x2 = 0;
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            EquationSolution(ref x1, ref x2, a, b, c);
+
+            Console.WriteLine($"Первый корень: {x1}\nВторой корень: {x2}");
+            Console.ReadKey();
+        }
+
+        public static double EquationSolution(ref double x1, ref double x2, int a, int b, int c)
+        {
+            double d = b * b - 4 * a * c;
+
+            if (d > 0)
+            {
+                d = Math.Sqrt(d);
+
+                x1 = (-b + d) / (2 * a);
+                x2 = (-b - d) / (2 * a);
+
+            }
+
+            return x1;
+        }
+    }
+}
